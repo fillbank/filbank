49fab32e25d3bb7126e0144d4ff7d02d27188ee5
fillbank <ivanfilcukov1@gmail.com>
2026-06-13 15:39:37 +0300

    Add WalletModalProvider with Phantom/Solflare adapters

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,30 @@
-import { StrictMode } from 'react'
+import { StrictMode, useMemo } from 'react'
 import { createRoot } from 'react-dom/client'
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react'
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui'
+import { PhantomWalletAdapter } from '@solana/wallet-adapter-phantom'
+import { SolflareWalletAdapter } from '@solana/wallet-adapter-solflare'
+import '@solana/wallet-adapter-react-ui/styles.css'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+function Root() {
+  const wallets = useMemo(() => [
+    new PhantomWalletAdapter(),
+    new SolflareWalletAdapter(),
+  ], [])
+
+  return (
+    <StrictMode>
+      <ConnectionProvider endpoint="https://api.mainnet-beta.solana.com">
+        <WalletProvider wallets={wallets} autoConnect>
+          <WalletModalProvider>
+            <App />
+          </WalletModalProvider>
+        </WalletProvider>
+      </ConnectionProvider>
+    </StrictMode>
+  )
+}
+
+createRoot(document.getElementById('root')).render(<Root />)
